f4a7921df020c038496dd5f7c44d2ee947eda64c
Cristian <Cristian@Cristian-PC>
2013-06-18 15:59:59 -0500

    Avance en creacion de formulario de insercion de mensajes programados

--- MAMi/src/java/DAO/DaoMenProgramado.java
+++ MAMi/src/java/DAO/DaoMenProgramado.java
@@ -4,12 +4,45 @@
  */
 package DAO;
 
+import Conn.Conn;
 import Modelo.MenProgramado;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
- * @author Juan David
+ * @author Cristian david
  */
 public class DaoMenProgramado extends MenProgramado{
-    
+    Conn conn = new Conn();
+    int id;
+
+    public int insertar(String Id, String Semana, String Mensaje, String Tipo) {
+        try {
+            id += 1;
+            String sql = "INSERT INTO MensajesProgramados VALUES (" + Id + " , '" + Semana + "' ,'" + Mensaje + "','" + Tipo + "')";
+            System.out.println("sql=" + sql);
+            conn.insertar(sql);
+            conn.getCon().close();
+            return 1;
+        } catch (SQLException ex) {
+            System.out.println("ex = " + ex.getMessage());
+            return -1;
+        }
+    }
+
+    public int Obtener_Secuencia() {
+        try {
+            int id1 = 0;
+            String sql = "select count(*) from MensajesProgramados";
+            ResultSet rs;
+            if ((rs = conn.consultar(sql)).next()) {
+                id1 = Integer.parseInt(rs.getString(1));
+            }
+            return id1;
+        } catch (SQLException ex) {
+            System.out.println("ex = " + ex.getMessage());
+        }
+        return 0;
+    }    
 }
